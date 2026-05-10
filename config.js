ceee2f3ea53fa5e095a3f6bcd8c11b81f216e9c1
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-05-10 18:58:29 -0400

    config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-const TIENDA = {
-    nombre: "SiReBAi",
-    rif: "291092.270890",
-    moneda: "Bs.",
-    logo: "logo.png",
-    colorPrimario: "#8B5CF6",
-    colorSecundario: "#6D28D9",
-    webhookUrl: "https://TU-N8N.com/webhook/siembra-webhook",
-    firebase: {
-        apiKey: "AIzaSyDiqM2oykoa-EkTJoFPQSdejjqRthdWxQY",
-        authDomain: "saas-universal-app.firebaseapp.com",
-        projectId: "saas-universal-app",
-        storageBucket: "saas-universal-app.firebasestorage.app",
-        messagingSenderId: "1058808192755",
-        appId: "1:1058808192755:web:a1ebeb9a3625f42cd56186"
-    }
-};
+
